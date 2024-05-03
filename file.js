70c4b6ed3259c1b20c3689dfd5e8d66d52eec4d6
Abby Harmony <118316272+abbyharmony@users.noreply.github.com>
2024-05-03 14:45:56 -0400

    Create calculator functions

---
+++ file.js
@@ -0,0 +1,31 @@
+// Create functions to add, subtract, multiply and divide
+function add(a, b) {
+    console.log(a + b);
+}
+
+function subtract(a, b) {
+    console.log(b - a);
+}
+
+function multiply(a, b) {
+    console.log(a * b);
+  };
+
+function divide(a, b) {
+    console.log(b / a);
+};
+
+ add(1, 2);
+ subtract(1, 2);
+ multiply(1, 2);
+ divide(1, 2);
+
+ let numA;
+ let operator;
+ let numB;
+
+ function operate(a, b) {
+    multiply(a, b);
+ };
+
+ 
