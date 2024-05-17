d51fe85645d4c8b0745694e9a0a5082066874ac4
Abby Harmony <118316272+abbyharmony@users.noreply.github.com>
2024-05-16 22:26:13 -0400

    Update operator function to be able to convert a string to a number and create appendNumber function

--- file.js
+++ file.js
@@ -18,25 +18,39 @@ function divide(a, b) {
     return a / b;
 }
 
- let numA;
- let operator;
- let numB;
+//create variables to store numbers, operator and display values
+ let numA = "";
+ let operator = "";
+ let numB = "";
+ let displayValue = "";
 
- function operate(operator, numA, numB) {
+//create a function that will be used to activate math functions above
+function operate(operator, numA, numB) {
+    const a = parseFloat(numA);
+    const b = parseFloat(numB);
     switch(operator) {
         case "+":
-            return add(numA, numB);
+            return add(a, b);
         case "-":
-            return subtract(numA, numB);
+            return subtract(a, b);
         case "*":
-            return multiply(numA, numB);
+            return multiply(a, b);
         case "/":
-            return divide(numA, numB);
+            return divide(a, b);
         default:
             throw new Error("error");
     }
- };
+};
 
+ /*create a function that will populate the display when the number buttons are clicked
+ and store the display value */
+ function appendNumber(number) {
+    displayValue += number;
+    document.getElementById('display').value = displayValue;
+};
+
+
+//Create event listeners for math buttons
  let addButton = document.getElementById("add");
  addButton.addEventListener("click", () => {
     add();
